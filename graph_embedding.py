8a9f8a98613d6c4ef527de459af8645d01bc9523
Philipp Skavantzos <philipp.skavantzos@gmail.com>
2024-07-19 16:13:07 +1200

    Add files via upload

--- graph_embedding.py
+++ graph_embedding.py
@@ -7,34 +7,41 @@ Original file is located at
     https://colab.research.google.com/drive/1Co_aofH5liRg-xLdCBJpP0F0w16SkrsM
 """
 
+# Import required packages
+
 import networkx as nx
 import random
+#%pip install gensim
 from gensim.models import Word2Vec
+import numpy as np
+import matplotlib.pyplot as plt
 import time
 
-n = 20
-p = 0.15
+# Create and draw graph
+
+n = 10
+p = 0.25
 
 G = nx.erdos_renyi_graph(n, p, directed = True)
 
-nx.draw(G, with_labels = True)
+# outdated version
+# nx.draw(G, with_labels = True)
 
-# idea:
-# create graph and copy graph and make slight adjustments, compare both embeddings and caclulate distance
-# have block using skipgram and alternative block using skipgram implementation from scratch (check geeksforgeeks for this, in separate python file)
+nx.draw_networkx(G, with_labels = True)
+plt.show()
 
-# adjust signature for graph type
+# perform random walks in graph
 
-def random_walk(graph, start_node:int = 0, walk_length:int = 1) -> list[int]:
+def random_walk(graph:nx.Graph, start_node:int = 0, walk_length:int = 1) -> list[int]:
 
-    sequence = [start_node]
+    sequence = [str(start_node)]
 
     for _ in range(walk_length):
         neighbours = [neighbour for neighbour in graph.neighbors(start_node)]
         if neighbours == []:
           return sequence
         selected_neighbour = random.choice(neighbours)
-        sequence.append(selected_neighbour)
+        sequence.append(str(selected_neighbour))
         start_node = selected_neighbour
 
     return sequence
@@ -45,8 +52,9 @@ for _ in range(5):
 
 # create walks for Word2Vec
 
-amount_walks = 5
+amount_walks = 200
 length_per_walk = 20
+dimension = 2
 
 walks = []
 
@@ -54,17 +62,28 @@ for _ in range(amount_walks):
   walks.append(random_walk(G, random.randrange(n), length_per_walk))
 
 
-# play around with Word2Vec hyperparameters
+# Train Word2Vec model, play around with Word2Vec hyperparameters
 
-model = Word2Vec(walks, vector_size = amount_walks, window = 2, sg = 1, min_count = 1)
+model = Word2Vec(walks, vector_size = dimension, window = 2, sg = 1, min_count = 1)
 model.train(walks, total_examples = model.corpus_count, epochs = 30, report_delay = 1)
 
-# show results of Deepwalk implementation
+# show results of node embedding
 
 for node in range(n):
   print(f"Node {node:2d}: {model.wv.get_vector(node)}")
 
-# testing / add clr screen, etc to it
+"""
+# compare to DeepWalk implementation from Karateclub
+
+karate_model = DeepWalk(walk_length = length_per_walk, dimensions = dimension, window_size = 2)
+karate_model.fit(G)
+karate_embedding = karate_model.get_embedding()
+
+for node in range(n):
+  print(f"Node {node:2d}: {karate_embedding[node]}")
+  """
+
+# testing for similarity of nodes
 
 input_number = 0
 
